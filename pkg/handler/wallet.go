7450b20d99636d29ed06f7eb86b1e5d4cd4c4570
salihkemaloglu <salihkemaloglu@gmail.com>
2021-09-17 15:41:50 +0700

    refactor(btc): handle same amount that have same time

--- pkg/handler/wallet.go
+++ pkg/handler/wallet.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -35,7 +36,7 @@ func (h *Handler) InsertTransaction(ctx *gin.Context) {
 
 		transaction := transactions[len(transactions)-1]
 		transaction.Datetime = t.Datetime
-		transaction.Amount += t.Amount
+		transaction.Amount = t.Amount
 		transactions = append(transactions, transaction)
 		h.cache.Set(walletID, transactions, cache.NoExpiration)
 	} else {
@@ -59,8 +60,8 @@ func (h *Handler) GetTransactions(ctx *gin.Context) {
 			httputil.NewError(ctx, http.StatusInternalServerError, errors.New("couldn't parse object"))
 			return
 		}
-		th := getTransactionsInBetween(t, transactions)
-		ctx.JSON(http.StatusOK, th)
+		ths := getTransactionsInBetween(t, transactions)
+		ctx.JSON(http.StatusOK, ths)
 		return
 	}
 
@@ -69,19 +70,40 @@ func (h *Handler) GetTransactions(ctx *gin.Context) {
 
 // getTransactionsInBetween return transactions between start and end time
 func getTransactionsInBetween(t model.Transaction, transactions []model.Transaction) []model.TransactionHistory {
-	th := []model.TransactionHistory{}
-	hour := 0
-	for _, transaction := range transactions {
-		if inTimeSpan(t.StartDatetime, t.EndDatetime, transaction.Datetime) {
-			if hour == 0 || transaction.Datetime.Hour() != hour {
-				hour = transaction.Datetime.Hour()
-				transaction.Datetime = time.Date(transaction.Datetime.Year(), transaction.Datetime.Month(), transaction.Datetime.Day(),
-					transaction.Datetime.Hour(), 0, 0, 0, transaction.Datetime.Location())
+	sort.Slice(transactions, func(i, j int) bool {
+		return transactions[i].Datetime.Before(transactions[j].Datetime)
+	})
+
+	ths := []model.TransactionHistory{}
+	year, month, day, hour := 0, 0, 0, 0
+	var totalAmount float64 = 0
+	for i := range transactions {
+		if inTimeSpan(t.StartDatetime, t.EndDatetime, transactions[i].Datetime) {
+			totalAmount += transactions[i].Amount
+			if hour == 0 || transactions[i].Datetime.Year() != year || int(transactions[i].Datetime.Month()) != month ||
+				transactions[i].Datetime.Day() != day || transactions[i].Datetime.Hour() != hour {
+				year, month, day, hour = transactions[i].Datetime.Year(), int(transactions[i].Datetime.Month()), transactions[i].Datetime.Day(),
+					transactions[i].Datetime.Hour()
+				datetime := time.Date(transactions[i].Datetime.Year(), transactions[i].Datetime.Month(), transactions[i].Datetime.Day(),
+					transactions[i].Datetime.Hour(), 0, 0, 0, transactions[i].Datetime.Location())
+				ths = append(ths, model.TransactionHistory{Datetime: datetime, Amount: totalAmount})
+			} else if transactions[i].Datetime.Year() == year || int(transactions[i].Datetime.Month()) == month ||
+				transactions[i].Datetime.Day() == day || transactions[i].Datetime.Hour() == hour {
+				setAmount(totalAmount, transactions[i], ths)
 			}
-			th = append(th, model.TransactionHistory{Datetime: transaction.Datetime, Amount: transaction.Amount})
+
 		}
 	}
-	return th
+	return ths
+}
+
+// setAmount sets amount of transaction that are same hour
+func setAmount(totalAmount float64, transaction model.Transaction, ths []model.TransactionHistory) {
+	for i := range ths {
+		if ths[i].Datetime.Hour() == transaction.Datetime.Hour() {
+			ths[i].Amount = totalAmount
+		}
+	}
 }
 
 // inTimeSpan checks time is between start and end
